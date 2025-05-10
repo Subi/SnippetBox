788431590f4f235bc6353e5f0a980e0fb27d260d
Subi <negus.dev@gmail.com>
2025-05-10 16:19:31 -0400

    add .env file and static files to improve ui

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	addr := os.Getenv("SNIPPETBOX_ADDR")
+
+	// Create a new mux server
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on :4000")
+	log.Printf("Starting server on %s", addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err = http.ListenAndServe(addr, mux)
 	log.Fatal(err)
 }
