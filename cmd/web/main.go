111830bcb9b258c013b5416a789f0738c114a9f3
Subi <subi@Judaeas-MBP.lan>
2025-05-11 14:14:28 -0400

    configure and setup database and connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
@@ -24,24 +26,27 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// Create db connection pool
+
+	db, err := sql.Open("mysql", os.Getenv("DB_ADDR"))
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	// Create a new mux server
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := &http.Server{
 		Addr:     addr,
-		Handler:  mux,
+		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
 	infoLog.Printf("Starting server on %s", addr)
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/subi/snippetbox
 
 go 1.24
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
