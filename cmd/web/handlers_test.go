274e8b38f6e55b66dbc819ad6465c582777871c5
Subi <subi@Judaeas-MacBook-Pro.local>
2025-05-20 11:23:06 -0400

    yay more test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"github.com/subi/snippetbox/pkg/models"
 	"net/http"
 	"testing"
+	"time"
 )
+
+var mockSnippet = &models.Snippet{
+	ID:      1,
+	Title:   "An old silent pond",
+	Content: "An old silent pond...",
+	Created: time.Now(),
+	Expires: time.Now(),
+}
 
 func TestPing(t *testing.T) {
 	app := newTestApplication(t)
@@ -16,4 +27,67 @@ func TestPing(t *testing.T) {
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
 	}
+}
+
+func TestShowSnippet(t *testing.T) {
+	t.Parallel()
+	// Create a new instance of our application struct which uses the mocked
+	// dependencies.
+	app := newTestApplication(t)
+
+	// Establish a new test server for running end-to-end tests.
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	// Set up some table-driven tests to check the responses sent by our
+	// application for different URLS
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+
+	for _, tt := range tests {
+		// rebind tt into this lexical scope to avoid concurrency bug from running
+		// sub-tests
+		//tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			// t.Parallel() // can't run in parallel with current testServer implementation
+
+			code, _, body := ts.get(t, tt.urlPath)
+			t.Logf("testing %q for want-code %d and want-body %q", tt.name, tt.wantCode,
+				tt.wantBody)
+
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contain %q, but got %q", tt.wantBody, body)
+			}
+		})
+	}
+}
+
+func TestSignupUser(t *testing.T) {
+	// Create the application struct containing our mocked dependencies and set
+	// up the test server for running and end-to-end test.
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	// Make a GET /user/signup request and then extract the CSRF token from the
+	// response body.
+	_, _, body := ts.get(t, "/user/signup")
+	csrfToken := extractCSRFToken(t, body)
+	// Log the CSRF token value in our test output.
+	t.Log(csrfToken)
 }
--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -3,15 +3,33 @@ package main
 import (
 	"github.com/golangcollege/sessions"
 	"github.com/subi/snippetbox/pkg/models/mock"
+	"html"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 )
+
+// Define a regular expression which captures the CSRF token value from the HTML for our
+// user signup page.
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	// Use the FindSubmatch method to extract the token from the HTML body.
+	// Note that this returns an array with the entire matched pattern in the first
+	// position, and th values of any captured data in the subsequent positions.
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	return html.UnescapeString(string(matches[1]))
+}
 
 // Create a newTestApplication helper which returns an instance of our
 // application struct containing mocked dependencies.
@@ -83,5 +101,29 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byt
 		t.Fatal(err)
 	}
 
+	return rs.StatusCode, rs.Header, body
+}
+
+// postForm sends POST requests to the test server. The final parameter to this method is a
+// url.Values object which can contain any data that you want to send in the request body.
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the response body.
+	defer func() {
+		if err := rs.Body.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Return the response status, headers, and body.
 	return rs.StatusCode, rs.Header, body
 }
